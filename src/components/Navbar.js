667b233a4d614e10d0b1fd9c053217aafe73f8df
Looper <aayush.bhattacharjee2002@gmail.com>
2023-08-26 22:55:21 +0530

    Made routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
       <nav className={`navbar navbar-expand-lg navbar-dark bg-dark`}>
         <div className="container-fluid">
-          <a className="navbar-brand" to="/">
+          <Link className="navbar-brand" to="/">
             News APP
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,49 +22,40 @@ export default class Navbar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="/about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/about">
-                  General
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" to="/business">
                   Business
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" to="/entertainment">
                   entertainment
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" to="/science">
                   science
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" to="/health">
                   health
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" to="/sports">
                   sports
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" to="/tech">
                   technology
-                </a>
+                </Link>
               </li>
             </ul>
             <div className="form-check form-switch text-dark">
